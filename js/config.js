9cdd0af21bb7a9d767d293cb0bac1bdfdb5e06f2
hoang tung <tunght7@bidv.com.vn>
2026-06-10 11:10:11 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyCaszPLa3M0HpR4AZ-3QKIxo-HbvkYv8rlDvchcRQAJFufqFwMTAPWMVk5_iIwjprmdA/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzTCIkkCBOSQyP2f_JBlZleM2GQXnnVSSgwdgR0pca473o71YIRDn9WfCcd7X_3dzW1uQ/exec';
 const WC_COMPETITION_CODE = 'WC';
 const WC_SEASON = 2026;
