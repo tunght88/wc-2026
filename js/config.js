e32bb699ad06cbd32415cedc894df452bde05518
tunght88 <tung.hoangthanh1988@gmail.com>
2026-06-30 20:44:59 +0700

    Update APPS_SCRIPT_URL to new endpoint

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxerVTSw2HIbB0CJ0frAnIfZKGiZzJ81n28pVQGFTKMPJnd4371w1UzSnDM6SHW_I3Okw/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxQfYjQRluHMz-ZSXwyNu55EkzAaGGs62MhZXYzEOVrCvT8hz3THsC36mWmgU996gSgog/exec';
 const WC_COMPETITION_CODE = 'WC';
 const WC_SEASON = 2026;
