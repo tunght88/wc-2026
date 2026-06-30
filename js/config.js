a97119af6d030290d261715db16ec11fb2ee97e2
tunght88 <tung.hoangthanh1988@gmail.com>
2026-06-30 21:12:36 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxQfYjQRluHMz-ZSXwyNu55EkzAaGGs62MhZXYzEOVrCvT8hz3THsC36mWmgU996gSgog/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzgqtHNV-Y21v7dSGFHZdy-Nb9W72dKiRDxXyY8UQOcyIYajN4ZXXEY3SRs7PL_dAZWMw/exec';
 const WC_COMPETITION_CODE = 'WC';
 const WC_SEASON = 2026;
